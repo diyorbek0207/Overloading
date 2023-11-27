cab0618987249d65b545830b74e1557066501914
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-11-27 15:40:22 +0500

    add Peremetr

--- over/src/constructor/Peremetr.java
+++ over/src/constructor/Peremetr.java
@@ -1,2 +1,38 @@
-package constructor;public class Peremetr {
+package constructor;
+
+public class Peremetr {
+    private int a=5;
+    private int b=7;
+    private int c=10;
+
+    public void P(){
+        int p=a+b+c;
+        System.out.println(p);
+    }
+
+    public void A(){
+        double A=a+b+c;
+        System.out.println(A);
+    }
+
+    public void B(){
+        float B=a+b+c;
+        System.out.println(B);
+    }
+
+    public void C(){
+        long C=a+b+c;
+        System.out.println(C);
+    }
+}
+
+class D{
+
+    public static void main(String[] args) {
+        Peremetr perimetr=new Peremetr();
+        perimetr.P();
+        perimetr.A();
+        perimetr.B();
+        perimetr.C();
+    }
 }
